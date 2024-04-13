d9f4a384f8baf9ea29d3d4438084451c62d7222c
divyakumaravel <divyakk98@gmail.com>
2024-04-13 21:10:42 +0530

    Builder pattern. Build home

--- src/main/java/com/example/designpatterns/builderMethod/BuilderPatternImpl.java
+++ src/main/java/com/example/designpatterns/builderMethod/BuilderPatternImpl.java
@@ -4,20 +4,43 @@ import com.example.designpatterns.builderMethod.employee.Employee;
 import com.example.designpatterns.builderMethod.employee.EmployeeDTO;
 import com.example.designpatterns.builderMethod.employee.EmployeeDTOBuilder;
 import com.example.designpatterns.builderMethod.employee.EmployeeWebDTOBuilder;
+import com.example.designpatterns.builderMethod.home.Home;
+import com.example.designpatterns.builderMethod.home.HomeDTO;
+import com.example.designpatterns.builderMethod.home.HomeDTOBuilder;
+import com.example.designpatterns.builderMethod.home.MyHomeDTOBuilder;
+
+import java.util.Objects;
 
 public class BuilderPatternImpl {
 
     public static void main(String[] args) {
-        Employee employee = getEmployee();
-        EmployeeDTOBuilder employeeDTOBuilder = new EmployeeWebDTOBuilder();
+        try {
+            Employee employee = getEmployee();
+            EmployeeDTOBuilder employeeDTOBuilder = new EmployeeWebDTOBuilder();
 
-        EmployeeDTO employeeDTO = directBuild(employee, employeeDTOBuilder);
-        System.out.println(employeeDTO);
+            EmployeeDTO employeeDTO = directBuild(employee, employeeDTOBuilder);
+            System.out.println(employeeDTO);
+
+            Home home = getHome();
+            HomeDTO homeDTO = directBuild(home, new MyHomeDTOBuilder());
+            System.out.println(homeDTO);
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
     }
 
     public static EmployeeDTO directBuild(Employee employee, EmployeeDTOBuilder employeeDTOBuilder) {
         return employeeDTOBuilder.withFirstName(employee.getFirstName())
                 .withLastName(employee.getLastName()).withAge(employee.getAge())
+                .build();
+    }
+
+    public static HomeDTO directBuild(Home home, HomeDTOBuilder homeDTOBuilder) {
+        return homeDTOBuilder.buildDoor(home.getDoor())
+                .buildWalls(home.getWalls())
+                .buildWindow(home.getWindow())
+                .buildPool(home.getPool())
+                .buildGarage(home.getGarage())
                 .build();
     }
 
@@ -27,5 +50,15 @@ public class BuilderPatternImpl {
         employee.setLastName("K");
         employee.setAge(25);
         return employee;
+    }
+
+    public static Home getHome() {
+        Home home = new Home();
+        home.setDoor("door");
+        home.setWalls("Walls");
+        home.setWindow("Windows");
+        home.setGarage("Garage");
+        home.setPool("Pool");
+        return home;
     }
 }
---
+++ src/main/java/com/example/designpatterns/builderMethod/home/HomeDTO.java
@@ -0,0 +1,16 @@
+package com.example.designpatterns.builderMethod.home;
+
+public interface HomeDTO {
+
+    String getWalls();
+
+    String getDoor();
+
+    String getWindow();
+
+    String getGarage();
+
+    String getPool();
+
+    String getGym();
+}
