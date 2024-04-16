25324749355919890ed7f2d41d9635685c5fa58a
divyakumaravel <divyakk98@gmail.com>
2024-04-16 16:11:33 +0530

    Facade pattern

---
+++ src/main/java/com/example/designpatterns/structuralPatterns/facadePattern/FacadePatternImpl.java
@@ -0,0 +1,8 @@
+package com.example.designpatterns.structuralPatterns.facadePattern;
+
+/*
+* Facade solves a problem where a client is tightly coupled with interface and classes of a subsystem, i.e.,
+* client has to interact with a large number of interfaces and classes in a subsystem to get result
+* */
+public class FacadePatternImpl {
+}
